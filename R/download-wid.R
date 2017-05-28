63248a250a20fc4e9cd57fac17a4d3251b0e7fa5
Thomas Blanchet <thomas.blanchet@ensae.org>
2017-05-28 22:48:34 +0200

    Added first server request

--- R/download-wid.R
+++ R/download-wid.R
@@ -21,8 +21,40 @@
 #' \code{"all"} for all population types. See 'Details' for more.
 #' @param metadata Should the function fetch metadata too (ie. variable
 #' descriptions, sources, methodological notes, etc.)? Default is \code{FALSE}.
+#' @param verbose Should the function indicate the progress of the request?
+#' Default is \code{FALSE}.
+#'
+#' @importFrom httr GET add_headers content
+#' @importFrom utils read.csv
+#'
+#' @export
 
 download_wid <- function(indicators="all", areas="all", years="all", ages="all",
-                         populations="all", metadata=FALSE) {
-    return(NULL)
+                         populations="all", metadata=FALSE, verbose=FALSE) {
+
+    # Make sure that at least some indicators and some areas were selected
+    if (indicators == "all" && areas == "all") {
+        stop("You must select at least some specific indicators, areas, or both.")
+    }
+
+    # Concatenate area codes, if necessary
+    if (areas != "all") {
+        query_areas <- paste(areas, collapse=",")
+    }
+
+    if (verbose) {
+        cat("* Get variables associated to your selection...")
+    }
+
+    # Get the variables associated to the area(s)
+    url <- paste0(
+        "https://rfap9nitz6.execute-api.eu-west-1.amazonaws.com/prod/",
+        "wid-countries-available-variables?countries=", areas, "&variables=all"
+    )
+    response_request <- GET(url, add_headers("x-api-key"=api_key))
+    response_content <- eval(parse(text=content(response_request, as="text", encoding="UTF-8")), envir=NULL)
+    response_table <- read.csv(text=response_content, stringsAsFactors=FALSE,
+        colClasses=c("character", "character", "character", "integer", "character"))
+
+    return(response_table)
 }
